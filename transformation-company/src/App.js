370f7d94d76736b0cba7204562960a5332162ae5
Raphael Andrade <raphael.andrade@ibm.com>
2020-08-06 11:35:36 -0300

    create react app. install scss and bootstrap

---
+++ transformation-company/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <>
+      <div className="stage">
+        Stage 
+      </div>
+    </>
+  );
+}
+
+export default App;
