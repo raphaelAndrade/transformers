99804b7ab5c4bd6ff367d17147d1aa73ac27fb5a
Raphael Andrade <raphael.andrade@ibm.com>
2020-08-08 12:32:20 -0300

    footer

--- transformation-company/src/App.js
+++ transformation-company/src/App.js
@@ -4,7 +4,8 @@ import Header from './Components/Layout/Header-Component';
 import Instructions from './Components/Layout/Instructions-Components';
 import Add_Fighter from './Components/Add_Fighter-Components';
 import List_Fighter_Battle from './Components/List_Fighter_Battle-Components';
-import Result_Battle from './Components/Result_Battle-Component'
+import Result_Battle from './Components/Result_Battle-Component';
+import Footer from './Components/Layout/Footer-Component';
 import { Provider } from './Components/Context/Context';
 
 import './App.scss';
@@ -18,6 +19,7 @@ function App() {
         <Add_Fighter />
         <List_Fighter_Battle />
         <Result_Battle />
+        <Footer />
       </>
     </Provider>
   );
---
+++ transformation-company/src/Components/Layout/Footer-Component.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => {
+ return(
+     <>
+       <footer>
+           <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <p>SOMETIMES EVEN THE WISEST OF MAN OR MACHINE CAN MAKE AN ERROR. - Optimus Prime</p>
+                    </div>
+                </div>
+           </div>
+       </footer>
+     </>
+ )
+}
+export default Footer;
