0a056369b77c91e5e2214026f3053d3378097744
Raphael Andrade <raphael.andrade@ibm.com>
2021-05-27 13:41:38 -0700

    add a background picture login page

--- transformation-company/src/Components/Layout/Header-Component.js
+++ transformation-company/src/Components/Layout/Header-Component.js
@@ -7,7 +7,7 @@ const Header = () => {
                 <div className="container">
                     <div className="row">
                         <div className="col-md-12">
-                            <h1>Transformers: War of Cybertron</h1>
+                            <h1>Transformers: War of Cybertron Vancouver</h1>
                         </div>
                     </div>
                 </div>
